bf24469e225763dd93fa901e212d4cef5467b400
JULIAN <juliandanielfranco@gmail.com>
2020-07-13 18:36:07 -0500

    feat: Add new logBox

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-
+require('react-native').unstable_enableLogBox()
 import client from './config/apollo';
 import { ApolloProvider } from '@apollo/client';
 
